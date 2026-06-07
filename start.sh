3bc36f1adafe48164f4f6aadec8f825ae229905c
Mike-Chandlerr <schakal1.mc@gmail.com>
2026-06-07 21:06:26 +0200

    Update start.sh to include model download and run ComfyUI

--- start.sh
+++ start.sh
@@ -4,8 +4,11 @@ cd /workspace
 export HF_HOME="/workspace/hf_cache"
 export HF_HUB_CACHE="/workspace/hf_cache"
 
+# 1. Modell-Download starten
 python3 /workspace/download_models.py
 
+# 2. Jupyter Lab im Hintergrund starten
 jupyter lab --allow-root --ip=0.0.0.0 --port=8888 --no-browser --NotebookApp.token='' --NotebookApp.password='' &
 
+# 3. ComfyUI im Vordergrund starten (Sicherer Direktaufruf aus /workspace)
 python3 main.py --listen 0.0.0.0 --port 8188 --highvram --fp8_e4m3fn-text-enc
