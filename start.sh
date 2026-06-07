d917edfa69f2cf4be65c48c1f5e694a7b1bb98b7
Mike-Chandlerr <schakal1.mc@gmail.com>
2026-06-07 17:47:51 +0200

    Remove unnecessary comments from start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-# In das Arbeitsverzeichnis wechseln
 cd /workspace
-
-# 1. Start des automatischen Modell-Downloads im Hintergrund
 python3 /workspace/download_models.py
-
-# 2. ComfyUI starten mit optimalen Argumenten für RTX 4090 / 5090
-# --listen 0.0.0.0 öffnet die Ports für das RunPod Web-Interface
-# --highvram nutzt die volle Power der 24GB+ Karten aus
-# --fp8_e4m3fn-text-enc spart massiv VRAM bei extrem großen Text-Encodern (wie bei Flux)
 python3 main.py --listen 0.0.0.0 --port 8188 --highvram --fp8_e4m3fn-text-enc
